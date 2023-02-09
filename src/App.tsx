d33356cabdbe865a9ba6b9d68ca1a065792202d5
Carlos <cbeckhauser@gmail.com>
2023-02-09 02:09:21 -0300

    Hero, navbar and Carousel section are done. Added slick react to carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import './App.css';
 
 import Hero from './components/Hero';
 import NavBar from './components/NavBar';
+import Carousel from './components/Carousel';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <div className='bg-black text-white m-auto antialised font-sans'>  
     <Hero/>
     <NavBar/>
+    <Carousel/>
+    <Carousel/>
+    <Carousel/>
     </div>
   );
 }
