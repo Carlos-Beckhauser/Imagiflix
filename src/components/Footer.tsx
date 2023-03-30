65a94375809b1f505a945a3fb121490a4ca162fe
Carlos <cbeckhauser@gmail.com>
2023-03-30 01:46:46 -0300

    fixed href from footer, added parentheses on new Date expression

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { faFacebookSquare, faInstagram, faTwitter, faYoutube } from "@fortawesom
 
 const Footer = () => {
 
-    const today = new Date;
+    const today = new Date();
 
     return (
     <footer className="mt-32 px-12 pb-4 text-gray-400 text-sm">
@@ -14,19 +14,19 @@ const Footer = () => {
         <FontAwesomeIcon className="mr-5 hover:text-white cursor-pointer" icon={faTwitter} size='2x'/>
         <FontAwesomeIcon  className="hover:text-white cursor-pointer" icon={faYoutube} size='2x'/>
         <div className="grid grid-cols-4 gap-2 my-8">          
-            <a className="hover:text-white" href="#">Idiomas e legendas</a>    
-            <a className="hover:text-white" href="#">Audiodescrição</a>    
-            <a className="hover:text-white" href="#">Centro de ajuda</a>    
-            <a className="hover:text-white" href="#">Cartão pré-pago</a>
-            <a className="hover:text-white" href="#">Imprensa</a>    
-            <a className="hover:text-white" href="#">Relação com investidores</a>    
-            <a className="hover:text-white" href="#">Carreiras</a>    
-            <a className="hover:text-white" href="#">Termos de uso</a>
-            <a className="hover:text-white" href="#">Privacidade</a>    
-            <a className="hover:text-white" href="#">Avisos legais</a>    
-            <a className="hover:text-white" href="#">Preferências de cookies</a>    
-            <a className="hover:text-white" href="#">Informações corporativas</a>
-            <a className="hover:text-white" href="#">Entre em contato</a>    
+            <a className="hover:text-white" href="#home">Idiomas e legendas</a>    
+            <a className="hover:text-white" href="#home">Audiodescrição</a>    
+            <a className="hover:text-white" href="#home">Centro de ajuda</a>    
+            <a className="hover:text-white" href="#home">Cartão pré-pago</a>
+            <a className="hover:text-white" href="#home">Imprensa</a>    
+            <a className="hover:text-white" href="#home">Relação com investidores</a>    
+            <a className="hover:text-white" href="#home">Carreiras</a>    
+            <a className="hover:text-white" href="#home">Termos de uso</a>
+            <a className="hover:text-white" href="#home">Privacidade</a>    
+            <a className="hover:text-white" href="#home">Avisos legais</a>    
+            <a className="hover:text-white" href="#home">Preferências de cookies</a>    
+            <a className="hover:text-white" href="#home">Informações corporativas</a>
+            <a className="hover:text-white" href="#home">Entre em contato</a>    
          </div>     
          <p>ⓒ 1997 - { today.getFullYear() } Imagiflix, Inc</p>
     </footer>
